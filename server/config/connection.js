77c6eecca5f1de92bf70d5b1c0f5a6823be5fada
royreid3rd <royreid3rd@gmail.com>
2024-04-09 02:32:47 -0400

    last test before bed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 // Load the Mongoose ORM
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Royreid3rd:mymongodbpassword@cluster0.28ltw2v.mongodb.net/');
 
 module.exports = mongoose.connection;
