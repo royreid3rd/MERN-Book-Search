fa3d83cfe9c6250fa9a9627549b09306df9c8024
royreid3rd <royreid3rd@gmail.com>
2024-04-09 01:07:28 -0400

    fixed all bugs. code should be ready to deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 // Load the Mongoose ORM
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
