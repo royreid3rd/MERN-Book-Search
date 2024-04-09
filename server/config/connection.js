d0a335169ff74c6f6ca962bcca985e2965a021bc
royreid3rd <royreid3rd@gmail.com>
2024-04-09 02:35:40 -0400

    last commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 // Load the Mongoose ORM
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Royreid3rd:mymongodbpassword@cluster0.28ltw2v.mongodb.net/');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
